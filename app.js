f9ca28859328d32f24e23b08e0c83ddda859ec74
JKoriginal <92023041+JKoriginal@users.noreply.github.com>
2023-12-04 19:38:33 +0530

    mongoose setuped

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -10,6 +11,11 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser : true});
+
+
+
 
 app.get("/", function (req, res){
     res.render("home");
