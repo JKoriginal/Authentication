179605656768720eeaec96782f4fbbe487f46ce2
JKoriginal <92023041+JKoriginal@users.noreply.github.com>
2023-12-05 10:21:23 +0530

    update with login and register route

--- app.js
+++ app.js
@@ -12,6 +12,15 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
+const userSchema = {
+    email : String,
+    password: String
+};
+
+const User = new mongoose.model("user", userSchema);
+
+
+
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser : true});
 
 
@@ -29,8 +38,38 @@ app.get("/register", function (req, res){
     res.render("register");
 });
 
+app.post("/register", function(req, res){
+    const newUser = new User({
+        email : req.body.username,
+        password: req.body.password
+    });
+    newUser.save()
+        .then(() => {
+            res.render("secrets");
+        })
+        .catch(err => {
+            console.error(err);
+            res.redirect("/register");
+        });
+});
 
+app.post("/login", function(req, res){
+    const username = req.body.username;
+    const password = req.body.password;
 
+    User.findOne({ email: username })
+        .then(foundUser => {
+            if (foundUser && foundUser.password === password) {
+                res.render("secrets");
+            } else {
+                res.redirect("/login"); // Redirect to login page if user not found or password is incorrect
+            }
+        })
+        .catch(err => {
+            console.error(err);
+            res.redirect("/login");
+        });
+});
 
 
 
@@ -39,4 +78,4 @@ app.get("/register", function (req, res){
 
 app.listen(3000, function() {
     console.log("Server started on port 3000.")
-})
+});
