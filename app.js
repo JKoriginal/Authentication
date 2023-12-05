e4fc86b4b83a2d8c9cdf3428e5ba076a85209f9d
JKoriginal <92023041+JKoriginal@users.noreply.github.com>
2023-12-05 11:56:08 +0530

    key secured

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //jshint esversion:6
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -6,6 +7,8 @@ const mongoose = require("mongoose");
 const encrypt = require("mongoose-encryption");
 
 const app = express();
+
+
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
@@ -18,8 +21,8 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-const secret = "Thisisourlittlesecret.";
-userSchema.plugin(encrypt, { secret : secret, encryptedFields: ["password"] });
+
+userSchema.plugin(encrypt, { secret : process.env.SECRET, encryptedFields: ["password"] });
 
 
 
