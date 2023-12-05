8e3ad91c04f588a33835cfa2ab94b68b1d1bb566
JKoriginal <92023041+JKoriginal@users.noreply.github.com>
2023-12-05 10:54:15 +0530

    database encyption with mongoose

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 
@@ -12,10 +13,15 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
     email : String,
     password: String
-};
+});
+
+const secret = "Thisisourlittlesecret.";
+userSchema.plugin(encrypt, { secret : secret, encryptedFields: ["password"] });
+
+
 
 const User = new mongoose.model("user", userSchema);
 
